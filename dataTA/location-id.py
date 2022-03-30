aa3eae5b3a3bfad7d0bfc96aa241e89579ee5935
@CintyaThen <cintyaclaudyathen@gmail.com>
2022-03-30 16:41:18 +0700

    update

--- dataTA/location-id.py
+++ dataTA/location-id.py
@@ -5,7 +5,18 @@ Created on Wed Mar 30 13:40:53 2022
 @author: WINDOWS_X
 """
 import pandas as pd # data processing
-read_file = pd.read_csv('areaid.csv')
+read_file = pd.read_csv('location.csv')
 df = pd.DataFrame(read_file)
-df.insert(1,"location",True)
-print(df)
+#df.replace(11,104)
+n = len(df['area id'].unique())
+#print(n)
+temp = df.replace({'area id':{35783:74, 5193:75, 32028:76, 20036:77, 29301:78, 29302:103}})
+#df.insert(1,"location",True)
+
+temp.to_csv('location.csv', index=False, encoding='utf-8')
+print(temp['area id'].unique())
+
+
+"""read_file = pd.read_csv('location.csv')
+df = pd.DataFrame(read_file)
+print(df['area id'].unique())"""
