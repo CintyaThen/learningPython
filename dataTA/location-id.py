b22328013389fc34d488467b3fc232931c6b53fa
@CintyaThen <cintyaclaudyathen@gmail.com>
2022-03-30 14:13:24 +0700

    up

--- dataTA/location-id.py
+++ dataTA/location-id.py
@@ -7,6 +7,5 @@ Created on Wed Mar 30 13:40:53 2022
 import pandas as pd # data processing
 read_file = pd.read_csv('areaid.csv')
 df = pd.DataFrame(read_file)
-#df.insert(1,"location",True)
-valueid = df['area id unique'].replace(['11'],'104')
+df.insert(1,"location",True)
 print(df)
