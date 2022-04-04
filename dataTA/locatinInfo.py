eddf69fe3c86995a9e375b6ae68cf78387d7a8ac
@CintyaThen <cintyaclaudyathen@gmail.com>
2022-04-04 17:28:37 +0700

    create

--- dataTA/locatinInfo.py
+++ dataTA/locatinInfo.py
@@ -6,12 +6,56 @@ Created on Fri Apr  1 13:57:24 2022
 """
 
 import pandas as pd # data processing
-read_file = pd.read_csv('location_dataset.csv')
-df = pd.DataFrame(read_file)
+#import dateutil
+df = pd.read_csv("location_dataset.csv",usecols=['start','area id','id_colums'], parse_dates=['start'])
+agregated = df.agg({'area id': list})
+grouped = df.set_index('start').groupby('start').resample('6H').sum()
+
+#df2 = df.set_index(pd.DatetimeIndex(df["start"])).drop("start", axis=1)
+#df = df.set_index('start').resample('6H').agg({'area id': list}).sum()
+#df = df.groupby([pd.Grouper( "id_colums"]).agg({'area id': list}).sum()
+#df.groupby(pd)
+#dfg = df.
+#df = df2.set_index('start')
+#df = df.groupby('id_colums').agg({'area id': list})
+#df2 = dfg.resample('6H').sum()
+print(grouped)
+"""agregated = df.agg({'area id': list})
+grouped = df.set_index('start').groupby('id_colums').resample('6H').sum()
+grouped.to_csv('locationInfo.csv',  encoding='utf-8')"""
+
+
+
+
+
+
+
+
+"""read_file = pd.read_csv('location_dataset.csv', usecols=['start','area id','id_colums'])
+time = 
+df = pd.DataFrame(data=read_file)
+#df = df.set_index(pd.DatetimeIndex(df['start']))
+a = [list(set(l)) for l in df['id_colums']]
+df.loc[:,'id_colums'] = a
+
+df = df.set_index('start').groupby('area id').a.resample('6H').sum()
+df = df.reset_index('id').reset_index()
+print(df)
+
+df1 =  pd.DataFrame(columns=['startNew', 'endNew'])
+df1['startNew']= pd.to_datetime(df1['startNew'])
+df1['endNew']= pd.to_datetime(df1['endNew'])
+
+for key, value in df.iterrows():
+    print(key, value)
+    startNew = value['start']
+    for date in pd.date_range(df['start'].min(), df['end'].max()):
+        df2"""
+
+
 #temp1 = df['start'].min()
 #temp2 = df['end'].max()
-df1 =  df[["start","end"]]
-df1['startNew'] = [pd.date_range(df1["start"].min(),df1["end"].max(), freq ='6H')]
-
-print(df1)
-
+#ind = pd.date_range(df["start"].min(), df["end"].max(), freq ='6H')
+#df = df.set_index(ind)
+#df.resample('W').sum()
+#
--- dataTA/location-id.py
+++ dataTA/location-id.py
@@ -5,12 +5,13 @@ Created on Wed Mar 30 13:40:53 2022
 @author: WINDOWS_X
 """
 import pandas as pd # data processing
-read_file = pd.read_excel('location_dataset.xlsx')
+"""read_file = pd.read_excel('location_dataset.xlsx')
 
 #df.replace(11,104)
 #n = len(df['area id'].unique())
 read_file.to_csv("location_dataset.csv", index=None, header=True)
 df = pd.DataFrame(pd.read_csv("location_dataset.csv"))
+
 print(df)
 #temp = df.replace({'area id':{35783:74, 5193:75, 32028:76, 20036:77, 29301:78, 29302:103}})
 #df.insert(1,"location",True)
@@ -19,6 +20,32 @@ print(df)
 #print(temp['area id'].unique())
 
 
-"""read_file = pd.read_csv('location.csv')
+read_file = pd.read_csv('location.csv')
 df = pd.DataFrame(read_file)
 print(df['area id'].unique())"""
+
+from random import randint
+
+time = pd.to_datetime( ['2016-01-01 00:00:00', '2016-01-01 05:00:00', 
+                        '2016-01-01 10:00:00', '2016-01-02 01:00:00', 
+                        '2016-01-02 04:00:00', '2016-01-01 02:00:00', 
+                        '2016-01-01 08:00:00', '2016-01-01 14:00:00',
+                        '2016-01-02 03:00:00' ]
+                      )
+
+id_1 = [1] * 5
+id_2 = [2] * 4
+
+lists = [0] * 9
+for i in range(9):
+    l = [randint(0,10)  for _ in range(randint(1,5) ) ]
+    l = list(set(l))
+    lists[i] = l
+
+data = {'timestamp': time, 'id': id_1 + id_2, 'lists': lists}
+example = pd.DataFrame(data=data)
+#a = [list(set(l)) for l in example.lists]
+#example.loc[:,'lists'] = a
+example.set_index('timestamp').groupby('id').lists.resample('D').sum()
+
+print(example)
