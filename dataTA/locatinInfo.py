0f8b217695e53610964188c5d23b2276670090b9
@CintyaThen <cintyaclaudyathen@gmail.com>
2022-04-07 17:02:30 +0700

    create

--- dataTA/locatinInfo.py
+++ dataTA/locatinInfo.py
@@ -6,56 +6,27 @@ Created on Fri Apr  1 13:57:24 2022
 """
 
 import pandas as pd # data processing
-#import dateutil
-df = pd.read_csv("location_dataset.csv",usecols=['start','area id','id_colums'], parse_dates=['start'])
-agregated = df.agg({'area id': list})
-grouped = df.set_index('start').groupby('start').resample('6H').sum()
+#import sysmpy as sy
+"""
+Slice data and list of area id
+df = pd.read_csv("location_dataset.csv",usecols=['start','area id','id_colums'],
+                                                 index_col='start', parse_dates=True)
 
-#df2 = df.set_index(pd.DatetimeIndex(df["start"])).drop("start", axis=1)
-#df = df.set_index('start').resample('6H').agg({'area id': list}).sum()
-#df = df.groupby([pd.Grouper( "id_colums"]).agg({'area id': list}).sum()
-#df.groupby(pd)
-#dfg = df.
-#df = df2.set_index('start')
-#df = df.groupby('id_colums').agg({'area id': list})
-#df2 = dfg.resample('6H').sum()
-print(grouped)
-"""agregated = df.agg({'area id': list})
-grouped = df.set_index('start').groupby('id_colums').resample('6H').sum()
-grouped.to_csv('locationInfo.csv',  encoding='utf-8')"""
+df = pd.DataFrame(df)
+#df.set_index(keys = ["start", "id_colums"])
+df = df.groupby([df.index.floor("6H"), "id_colums"]).agg({'area id': lambda x: list(set(x))})
+#print(df)
 
+df.to_csv('locationInfo.csv',  encoding='utf-8')"""
+"""
+df = df.groupby([df.index.floor("6H"), "id_colums"]).agg({'area id': lambda x: list(x)})
 
+df = pd.read_csv("locationInfo.csv")
+df = pd.DataFrame(df)
 
-
-
-
-
-
-"""read_file = pd.read_csv('location_dataset.csv', usecols=['start','area id','id_colums'])
-time = 
-df = pd.DataFrame(data=read_file)
-#df = df.set_index(pd.DatetimeIndex(df['start']))
-a = [list(set(l)) for l in df['id_colums']]
-df.loc[:,'id_colums'] = a
-
-df = df.set_index('start').groupby('area id').a.resample('6H').sum()
-df = df.reset_index('id').reset_index()
-print(df)
-
-df1 =  pd.DataFrame(columns=['startNew', 'endNew'])
-df1['startNew']= pd.to_datetime(df1['startNew'])
-df1['endNew']= pd.to_datetime(df1['endNew'])
-
-for key, value in df.iterrows():
-    print(key, value)
-    startNew = value['start']
-    for date in pd.date_range(df['start'].min(), df['end'].max()):
-        df2"""
-
-
-#temp1 = df['start'].min()
-#temp2 = df['end'].max()
-#ind = pd.date_range(df["start"].min(), df["end"].max(), freq ='6H')
-#df = df.set_index(ind)
-#df.resample('W').sum()
-#
+print(df.head())"""
+df = pd.read_csv("locationInfo.csv")
+#df = pd.DataFrame(df)
+#multi = df.set_index(['start','id_colums','area id'])
+newdf = pd.pivot_table(df,                             index=['start', 'id_colums'], columns='area id')
+print(newdf)
--- dataTA/untitled5.py
+++ dataTA/untitled5.py
@@ -1,35 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  5 13:53:59 2022
 
+@author: WINDOWS_X
+"""
 
-clear acqu_a acqu_b
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  5 13:19:10 2022
 
-ua = 2;
-ub = 6;
-uaid = map94_106(ua);
-ubid = map94_106(ub);
+@author: WINDOWS_X
+"""
 
-event_a = s(uaid).comm;
-acqu_a = zeros(1,1);
-c = 1;
-for i = 1:length(event_a)
-    tmp = event_a(i).hashNum;
-    if ~ isnan(tmp)
-        acqu_a(c) = tmp;
-        c = c + 1;
-    end
-end
-
-sum(acqu_a == s(ubid).my_hashedNumber)
-
-
-event_b = s(ubid).comm;
-acqu_b = zeros(1,1);
-c = 1;
-for i = 1:length(event_b)
-    tmp = event_b(i).hashNum;
-    if ~ isnan(tmp)
-        acqu_b(c) = tmp;
-        c = c + 1;
-    end
-end
-
-sum(acqu_b == s(uaid).my_hashedNumber)
+# Run this program on your local python
+# interpreter, provided you have installed
+# the required libraries.
+  
+# Importing the required packages
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+  
+# Function importing Dataset
+def importdata():
+    balance_data = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-'+
+'databases/balance-scale/balance-scale.data',
+    sep= ',', header = None)
+      
+    # Printing the dataswet shape
+    print ("Dataset Length: ", len(balance_data))
+    print ("Dataset Shape: ", balance_data.shape)
+      
+    # Printing the dataset obseravtions
+    print ("Dataset: ",balance_data.head())
+    return balance_data
+  
+# Function to split the dataset
+def splitdataset(balance_data):
+  
+    # Separating the target variable
+    X = balance_data.values[:, 1:5]
+    Y = balance_data.values[:, 0]
+  
+    # Splitting the dataset into train and test
+    X_train, X_test, y_train, y_test = train_test_split( 
+    X, Y, test_size = 0.3, random_state = 100)
+      
+    return X, Y, X_train, X_test, y_train, y_test
+      
+# Function to perform training with giniIndex.
+def train_using_gini(X_train, X_test, y_train):
+  
+    # Creating the classifier object
+    clf_gini = DecisionTreeClassifier(criterion = "gini",
+            random_state = 100,max_depth=3, min_samples_leaf=5)
+  
+    # Performing training
+    clf_gini.fit(X_train, y_train)
+    return clf_gini
+      
+# Function to perform training with entropy.
+def tarin_using_entropy(X_train, X_test, y_train):
+  
+    # Decision tree with entropy
+    clf_entropy = DecisionTreeClassifier(
+            criterion = "entropy", random_state = 100,
+            max_depth = 3, min_samples_leaf = 5)
+  
+    # Performing training
+    clf_entropy.fit(X_train, y_train)
+    return clf_entropy
+  
+  
+# Function to make predictions
+def prediction(X_test, clf_object):
+  
+    # Predicton on test with giniIndex
+    y_pred = clf_object.predict(X_test)
+    print("Predicted values:")
+    print(y_pred)
+    return y_pred
+      
+# Function to calculate accuracy
+def cal_accuracy(y_test, y_pred):
+      
+    print("Confusion Matrix: ",
+        confusion_matrix(y_test, y_pred))
+      
+    print ("Accuracy : ",
+    accuracy_score(y_test,y_pred)*100)
+      
+    print("Report : ",
+    classification_report(y_test, y_pred))
+  
+# Driver code
+def main():
+      
+    # Building Phase
+    data = importdata()
+    X, Y, X_train, X_test, y_train, y_test = splitdataset(data)
+    clf_gini = train_using_gini(X_train, X_test, y_train)
+    clf_entropy = tarin_using_entropy(X_train, X_test, y_train)
+      
+    # Operational Phase
+    print("Results Using Gini Index:")
+      
+    # Prediction using gini
+    y_pred_gini = prediction(X_test, clf_gini)
+    cal_accuracy(y_test, y_pred_gini)
+      
+    print("Results Using Entropy:")
+    # Prediction using entropy
+    y_pred_entropy = prediction(X_test, clf_entropy)
+    cal_accuracy(y_test, y_pred_entropy)
+      
+      
+# Calling main function
+if __name__=="__main__":
+    main()
