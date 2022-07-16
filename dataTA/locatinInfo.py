1f7ce8985693722f9e27e3d99d6b2047be1bc4b9
Cintya Then <cintyaclaudyathen@gmail.com>
2022-07-16 19:51:52 +0700

    Revert "create"
    This reverts commit 394d61d26ddfd22428784c496b9bdf762cd530c5.

--- dataTA/locatinInfo.py
+++ dataTA/locatinInfo.py
@@ -8,11 +8,6 @@ Created on Fri Apr  1 13:57:24 2022
 import pandas as pd # data processing
 import numpy as np
 #import sysmpy as sy
-df = pd.read_csv("location_dataset.csv", usecols=['start','end','id_colums'],parse_dates = ['start','end'])
-df = pd.DataFrame(df)
-df['intercontact'] = df.groupby(['id_colums','start','end']).apply(lambda x:x['start'] - x['end'].shift(1))
-df = df['intercontact']
-print(df)
 """
 Slice data and list of area id
 df = pd.read_csv("location_dataset.csv",usecols=['start','area id','id_colums'],
@@ -31,8 +26,8 @@ df = pd.read_csv("locationInfo.csv")
 df = pd.DataFrame(df)
 
 print(df.head())"""
-#df = pd.read_csv("locationInfo.csv", usecols=['area id'])
-#df = pd.DataFrame(df)
+df = pd.read_csv("locationInfo.csv", usecols=['area id'])
+df = pd.DataFrame(df)
 #df = df.set_index(['start','id_colums'])
 
 #def sumList(row):
@@ -43,6 +38,6 @@ print(df.head())"""
 #suml  = df['area id'].to_numpy()
 #df["sum"] = sum(suml)
 #df['area id'] = df['area id'].astype(str).astype(int)
-#es = np.sum(df['area id'].iloc[1])
-#print(res)
+res = np.sum(df['area id'].iloc[1])
+print(res)
 #df.to_csv('locationInfo.csv',       encoding='utf-8')
